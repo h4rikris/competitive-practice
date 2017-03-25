5e0447748b106d352e43f556e97a9f03f4cb950a
harikrishnakanchi <harikrishnakanchi@gmail.com>
2017-03-25 15:32:18 +0530

    linear complexity

--- zeroflip/zeroflip.py
+++ zeroflip/zeroflip.py
@@ -1,18 +1,25 @@
 l = map(int, raw_input().split())
 m = input()
-max_length = 0
-positions = []
-temp_positions = []
-
-for i in range(0, len(l)):
-    count = 0
-    temp_positions = []
-    for j in range(i, len(l)):
-        if l[j] == 0:
-            count = count + 1
-            temp_positions.append(j)
-        subarray_length = j - i + 1
-        if count == m and max_length < subarray_length:
-            positions = temp_positions[:]
-            max_length = subarray_length
-print positions
+left = 0
+right = 0
+numberOfZeroes = 0
+max_width = -1
+max_left = 0
+max_right = 0
+while right < len(l):
+    if numberOfZeroes <= m:
+        if l[right] == 0:
+            numberOfZeroes = numberOfZeroes + 1
+        right = right + 1
+    if numberOfZeroes > m:
+        if l[left] == 0:
+            numberOfZeroes = numberOfZeroes - 1
+        left = left + 1
+    width = right - left
+    if numberOfZeroes <= m and width > max_width:
+        max_width = width
+        max_left = left
+        max_right = right
+for i in range(max_left, max_right):
+    if l[i] == 0:
+        print i,
